63b2c3d419dc41a83be5b175b5c72564872e60ec
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 14:05:47 -0700

    Create Header component with initial styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
-
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
     <main className='App'>
-      <h1>MixMaster</h1>
+      <Header className='header'/>
     </main>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+
+  return (
+    <div className='header'>
+      <h1>MixMaster</h1>
+    </div>
+  )
+}
+
+export default Header
