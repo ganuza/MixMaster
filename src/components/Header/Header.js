f80c965944b2f5aad84ccd8e6ff14d89d75c3c8c
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 14:28:50 -0700

    Create NavLinks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
@@ -5,6 +6,11 @@ const Header = () => {
   return (
     <div className='header'>
       <h1>MixMaster</h1>
+      <div className='header-links-container'>
+        <NavLink className='home-link' to='/'>Home</NavLink>
+        <NavLink className='explore-link' to='/'>Choose Spirit</NavLink>
+      </div>
+      
     </div>
   )
 }
