db7bd24c8b41a9d1ab7fd409bcddab5a96abfc53
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 14:27:30 -0700

    Implement Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from '../Header/Header';
+import Welcome from '../Welcome/Welcome';
 import './App.css';
 
 function App() {
   return (
     <main className='App'>
       <Header className='header'/>
+      <Routes>
+        <Route path='/' element={
+          <Welcome />
+        }
+        />
+      </Routes>
     </main>
   )
 }
