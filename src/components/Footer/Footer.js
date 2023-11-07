2c4d8ba626018de2568dfaead2d6fbbfadf282bc
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 15:06:03 -0700

    Create Footer Component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+const Footer = () => {
+
+  return (
+    <div className='footer'>
+      {/* <h2>Footer</h2> */}
+      <div className='teal-stripe'></div>
+      <div className='red-stripe'></div>
+      <div className='orange-stripe'></div>
+      <div className='yellow-stripe'></div>
+    </div>
+    
+  )
+}
+
+export default Footer
