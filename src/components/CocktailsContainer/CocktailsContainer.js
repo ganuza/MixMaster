256393f48ec1d25d201511aea00f8bec1b214428
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 21:04:51 -0700

    Add functionality for setting cocktails from network request to state

--- src/components/CocktailsContainer/CocktailsContainer.js
+++ src/components/CocktailsContainer/CocktailsContainer.js
@@ -1,17 +1,37 @@
+import { useState, useEffect } from 'react'
 import Form from '../Form/Form'
 import './CocktailsContainer.css'
+import { getCocktails } from '../../apiCalls'
 
 const CocktailsContainer = () => {
+  const [cocktails, setCocktails] = useState([])
+  const [chosenSpirit, setChosenSpirit] = useState('')
+
+  const captureSpirit = (spirit) => {
+    setChosenSpirit(spirit)
+  }
+
+  console.log('chosenSpirit: ', chosenSpirit)
+
+  useEffect(() => {
+    if (chosenSpirit) {
+      getCocktails(chosenSpirit)
+        .then(data => {
+          setCocktails(data.drinks || []);
+        })
+        .catch(error => console.log(error));
+    }
+  }, [chosenSpirit]);
 
   return (
     <div className="cocktails-oval-container">
-    <div className="cocktails-oval cocktails-overlap-3"></div>
-    <div className="cocktails-oval cocktails-overlap-2"></div>
-    <div className="cocktails-oval cocktails-overlap-1"></div>
-    <div className="cocktails-selection-box">
-      <Form />
+      <div className="cocktails-oval cocktails-overlap-3"></div>
+      <div className="cocktails-oval cocktails-overlap-2"></div>
+      <div className="cocktails-oval cocktails-overlap-1"></div>
+      <div className="cocktails-selection-box">
+        <Form captureSpirit={captureSpirit}/>
+      </div>
     </div>
-  </div>
   )
 }
 
