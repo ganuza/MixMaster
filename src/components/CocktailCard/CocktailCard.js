06387a2de697c56255c4e2dc5f2667d689c5ecdf
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-08 11:24:28 -0700

    Add styling to cocktail cards

--- src/components/CocktailCard/CocktailCard.js
+++ src/components/CocktailCard/CocktailCard.js
@@ -3,9 +3,8 @@ import './CocktailCard.css'
 const CocktailCard = ({ cocktail }) => {
   return (
     <div className="cocktail-card">
+      <img className='cocktail-img' src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
       <h2>{cocktail.strDrink}</h2>
-      {/* <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} /> */}
-      {/* <p>{cocktail.strInstructions}</p> */}
     </div>
   );
 };
