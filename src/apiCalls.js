0d5c8c08e66e7b30882cc2aab8e23b3bd7697332
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 21:02:08 -0700

    Fix apiCall

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,8 @@
 export const getCocktails = (spirit) => {
-  return fetch(`www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spirit}`)
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spirit}`)
   .then(response => {
     if(!response.ok) {
-      throw new Error(`${statusCode} ${statusText}`)
+      throw new Error(`${response.status} ${response.statusText}`)
     }
     return response.json()
   })
