06955a31f2911848b02b2dae1d49f78f3508d601
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 18:11:20 -0700

    Change wording of message

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => {
     <div className="oval overlap-2"></div>
     <div className="oval overlap-1"></div>
     <div className="selection-box">
-      <h2 className='selection-box-text'>Welcome to MixMaster, where your favorite cocktail recipes are just a click away! Click on the Choose by Liquor button above to find recipes with your chosen spirit!</h2>
+      <h2 className='selection-box-text'>Welcome to MixMaster, where your favorite cocktail recipes are just a click away! Click on the Choose Spirit link above to find recipes with your chosen spirit!</h2>
     </div>
   </div>
   )
