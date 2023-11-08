da28c64596ee6afce2e9ac0a08a60140b6abd09b
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-08 11:47:56 -0700

    Add api call for selected cocktail recipe

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,13 @@ export const getCocktails = (spirit) => {
     return response.json()
   })
 }
+
+export const getRecipe = () => {
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+      return response.json()
+    })
+}
