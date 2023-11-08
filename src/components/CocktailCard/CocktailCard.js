93883a77e9788d25f0e9a64b79bc36aecfe35cc2
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-08 12:06:35 -0700

    Add functionality for setting the recipe state to data from network request

--- src/components/CocktailCard/CocktailCard.js
+++ src/components/CocktailCard/CocktailCard.js
@@ -1,6 +1,21 @@
+import { getRecipe } from '../../apiCalls';
+import { useState, useEffect } from 'react'
 import './CocktailCard.css'
 
 const CocktailCard = ({ cocktail }) => {
+
+  const [expanded, setExpanded] = useState(false)
+  const [recipe, setRecipe] = useState(null)
+  const [recipeFetchError, setRecipeFetchError] = useState('')
+
+  useEffect(() => {
+    if (expanded) {
+      getRecipe()
+        .then(data => setRecipe(data))
+        .catch(error => setRecipeFetchError(error.message))
+    } 
+  }, [expanded])
+
   return (
     <div className="cocktail-card">
       <img className='cocktail-img' src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
