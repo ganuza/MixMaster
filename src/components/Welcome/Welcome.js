30484273426a6dd4e498c4b1018a8f6608fa7b38
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-07 18:04:37 -0700

    Create functionality for rendering welcome message

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -3,7 +3,15 @@ import './Welcome.css'
 const Welcome = () => {
 
   return (
-    <h2>Hello from Welcome Component</h2>
+    
+    <div className="oval-container">
+    <div className="oval overlap-3"></div>
+    <div className="oval overlap-2"></div>
+    <div className="oval overlap-1"></div>
+    <div className="selection-box">
+      <h2 className='selection-box-text'>Welcome to MixMaster, where your favorite cocktail recipes are just a click away! Click on the Choose by Liquor button above to find recipes with your chosen spirit!</h2>
+    </div>
+  </div>
   )
 }
 
