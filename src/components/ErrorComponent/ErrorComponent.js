4b189410406ea9f1a920081c3c9c0af69ae2c11d
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-09 21:40:30 -0700

    Add structure for ErrorComponent styling

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -4,11 +4,21 @@ import './ErrorComponent.css'
 const ErrorComponent = ({error, message}) => {
 
   return (
+<div className="oval-container">
+    <div className="oval overlap-3"></div>
+    <div className="oval overlap-2"></div>
+    <div className="oval overlap-1"></div>
+    <div className="selection-box">
     <div className='error-container'>
       <h2 className='error-h2'>ERROR</h2>
       <h3 className='error-h3'>{error} </h3>
       <h3 className='error-h3'>{message}</h3>
     </div>
+    </div>
+  </div>
+
+
+    
   )
 }
 
