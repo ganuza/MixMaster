ac6e8085dabc897eaf4262ef921d99dcb6e67240
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-10 11:20:08 -0700

    Add proptypes and conditionally render Loading...

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -4,22 +4,24 @@ import './ErrorComponent.css'
 const ErrorComponent = ({error, message}) => {
 
   return (
-<div className="oval-container">
-    <div className="oval overlap-3"></div>
-    <div className="oval overlap-2"></div>
-    <div className="oval overlap-1"></div>
-    <div className="selection-box">
-    <div className='error-container'>
-      <h2 className='error-h2'>ERROR</h2>
-      <h3 className='error-h3'>{error} </h3>
-      <h3 className='error-h3'>{message}</h3>
-    </div>
-    </div>
-  </div>
-
-
-    
+    <div className="oval-container">
+        <div className="oval overlap-3"></div>
+        <div className="oval overlap-2"></div>
+        <div className="oval overlap-1"></div>
+        <div className="selection-box">
+          <div className='error-container'>
+            <h2 className='error-h2'>ERROR</h2>
+            <h3 className='error-h3'>{error} </h3>
+            <h3 className='error-h3'>{message}</h3>
+          </div>
+        </div>
+      </div>
   )
 }
 
 export default ErrorComponent
+
+ErrorComponent.propTypes = {
+  error: PropTypes.string, 
+  message: PropTypes.string
+}
